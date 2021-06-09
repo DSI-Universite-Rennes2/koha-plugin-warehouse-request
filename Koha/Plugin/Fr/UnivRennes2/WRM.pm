30c719d61931a0a43c2292d88aed2620177bc2a3
Nicolas Legrand <nicolas.legrand@bulac.fr>
2021-06-09 09:36:10 +0200

    Évite le surencodage utf8
    « Demandes des adhÃ©rents » au lieu de « Demandes des adhérents »

--- Koha/Plugin/Fr/UnivRennes2/WRM.pm
+++ Koha/Plugin/Fr/UnivRennes2/WRM.pm
@@ -351,7 +351,7 @@ sub opac_head {
 sub opac_js {
     my ($self) = @_;
 
-    return read_file( abs_path( $self->mbf_path('js/opac.js') ), encoding => 'utf8' );
+    return read_file( abs_path( $self->mbf_path('js/opac.js') ), { binmode => 'utf8' } );
 }
 
 sub intranet_head {
@@ -375,7 +375,7 @@ sub intranet_head {
 sub intranet_js {
     my ($self) = @_;
 
-    return read_file( abs_path( $self->mbf_path('js/intranet.js') ), encoding => 'utf8'  );
+    return read_file( abs_path( $self->mbf_path('js/intranet.js') ), { binmode => 'utf8' }  );
 }
 
 sub configure {
